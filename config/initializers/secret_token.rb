d15f6b4f386fc42731fe83f9eb326704347d01e8
Chris Edwards <ladiesmanjetsfan1992@hotmail.com>
2014-01-30 20:56:43 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pintresting::Application.config.secret_key_base = 'c8bc7fc296eebbb3f8e4f8f721e9e19ef4493aa1352a77303c416fc79352cc79dec8cbcef047c7999f2bcf2fcaf30a85cacc5a7b0bcc1a16a45a67d48e26f191'
